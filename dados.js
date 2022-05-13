ae467ef72b6823903d50b12e19c69bdf2043328f
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-13 18:59:34 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,30 @@
+// Anotações da Aula 05 Módulo B - Variáveis e Tipos Primitivos
+// Comenta o código em 1 linha
+
+/* Comenta o código em 
+mais de 1 linha */
+
+var n2 = 8.5 // dizemos que n2 (identificador) recebe 8.5 (atribuição)
+var a2 = null // null = vazio
+
+/* O identificador pode começar com:
+letra, $ ou _
+Não pode:
+começar com números ou letras maiúsculas, conter espaços, nomes desconexos
+ou palavras reservadas (ex: var)
+*/
+
+var tel = "(48)93845-8014"
+var nome = "Ana Beatriz"
+var salario = 6500.00
+
+// Variáveis servem para guardar dados. Exemplo:
+var numeros = [5,  18, -12] // tipo Number
+var nomes = ["Google", 'JavaScript', `Maria`] // tipo String
+var afirmacoes = [true, false] // tipo Boolean
+
+/* Typeof: identifica o tipo de dado (no node do console):
+Number (Infinity, NaN), Boolean, null, undefined, function, String, object (Array), etc.
+*/
+var n = 200
+typeof n // 'number'
