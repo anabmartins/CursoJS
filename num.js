071ebef6c9d908169f75cf80b801a721d2369746
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-15 23:29:10 -0300

    Add files via upload

---
+++ num.js
@@ -0,0 +1,4 @@
+var n1 = 1545.5
+n1.toFixed(2)
+n1.toFixed(2).replace('.',',')
+n1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
---
+++ string.js
@@ -0,0 +1,14 @@
+nome = "Ana"
+nota = 10
+idade = 16
+
+alert("A aluna " + nome + "com " + idade + " anos, tirou " + nota)
+
+var s = 'JavaScript'
+'Eu estou aprendendo s' // não faz interpolação
+'Eu estou aprendendo' + s // usa concatenação
+'Eu estou aprendendo ${s}' // usa template string
+
+s.length // quantos caracteres a string tem
+s.toUpperCase // tudo para 'MAIÚSCULAS'
+s.toLowerCase // tudo para 'minúsculas'
