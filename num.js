aab6c463a6acfe6132e9d981f3fdfe1fb3312ae6
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-16 17:32:02 -0300

    Update num.js

--- num.js
+++ num.js
@@ -1,4 +1,21 @@
-var n1 = 1545.5
-n1.toFixed(2)
-n1.toFixed(2).replace('.',',')
-n1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+var n1 = 1543.5
+
+n1.toFixed(2) //  fixa em duas casas decimais
+n1.toFixed(2).replace('.',',') // troca . por ,
+n1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) // localiza a moeda do Brasil para usar o $ no início
+
+var n = 3
+
+n += 4 // n = n + 4 
+n -= 5 // n = n - 5  
+n *= 4 // n = n * 4 
+n /= 2 // n = n / 2 
+n **= 2 // n = n ** 2
+n %= 5 // n = n % 5 
+
+var x = 5
+
+x ++ // + 1
+x -- // - 1
+
+console.log(x); // 4
