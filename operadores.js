ba71e4959a514bf9eadfb8bfb1e937e9f188d946
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-16 19:24:59 -0300

    Update operadores.js

--- operadores.js
+++ operadores.js
@@ -32,3 +32,13 @@ a <= b || b / 2 == 2 // true
 idade >= 15 && idade <= 17 // a idade está entre 15 e 17?
 estado == 'RJ' || estado == 'SP' // o estado é RJ ou SP?
 salario > 1500 && sexo != 'M' // o salário é acima de 1500 e não é homem?
+
+// ternário
+// teste ? true : false
+media>=7.0?"Aprovado":"Reprovado"
+
+var x = 8
+var res = x % 2 == 0 ? 5: 9 //5
+
+var idade = 19
+var r = idade >= 18 ? "Maior" : "Menor" // "Maior"
