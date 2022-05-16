6995b83106ffc8f6c1475e347f9e2714e1321b0c
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-16 19:11:06 -0300

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,23 @@
+5 > 2 // true
+7 < 4 // false
+8 >= 8 // true
+9 <= 7 // false
+5 == 5 // true
+4 != 4 // false
+
+preço >= 200.50 // o preço é maior ou igual a 200.50?
+idade < 18 // a idade é menor do que 18?
+curso == 'JavaScript' // o curso é JavaScript?
+n1 != n2 // o primeiro número é diferente do segundo?
+
+5 == 5 // true
+5 == '5' // true
+// operador de identidade (idêntico a)
+5 === '5' // false
+
+/*Lógicos
+! negação
+&& conjunção (e)
+|| dijunção (ou) */
+
+
