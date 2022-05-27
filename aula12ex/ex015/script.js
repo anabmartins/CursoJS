7eec94945260147ee943739e5ac3048cf69a89f5
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-27 17:23:22 -0600

    Add files via upload

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -9,12 +9,42 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked){
             gênero = 'Homem'
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'bebe-m.png')
+            } else if (idade < 21){
+                //jovem
+                img.setAttribute('src', 'jovem-m.png')
+            } else if(idade < 50){
+                //adulto
+                img.setAttribute('src', 'adulto-m.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso-m.png')
+            }
         } else if (fsex[1].checked){
             gênero = 'Mulher'
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'bebe-f.png')
+            } else if (idade < 21){
+                //jovem
+                img.setAttribute('src', 'jovem-f.img')
+            } else if (idade < 50){
+                //adulto
+                img.setAttribute('src', 'adulto-f.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso-f.png')
+            }
         }
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 
 }
