b711db1f2ca50bba0b9bf13eb03d64a583cd2c66
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-26 21:00:45 -0300

    Add files via upload

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,21 @@
-alert('Olá')
+function carregar(){
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('img')
+var data = new Date()
+var hora = data.getHours()
+
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) {
+    //BOMDIA
+    img.src = 'foto_dia.png'
+    document.body.style.background = '#C39F77'
+} else if (hora >= 12 && hora < 18){
+    //Boa tarde
+    img.src = 'foto_tarde.png'
+    document.body.style.background = '#DD660D'
+} else {
+    //bo noite
+    img.src = 'foto_noite.png'
+    document.body.style.background = '#1C4153'
+    }
+}
---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,20 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || Number(fano.value) > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        if (fsex[0].checked){
+            gênero = 'Homem'
+        } else if (fsex[1].checked){
+            gênero = 'Mulher'
+        }
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+    }
+
+}
