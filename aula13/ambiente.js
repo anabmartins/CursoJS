75a81412f95b95e99d1b7f5bb75cea6a1f875080
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-27 18:28:15 -0600

    Add files via upload

---
+++ aula13/ambiente.js
@@ -0,0 +1,30 @@
+// While - estrutura de repetição com teste lógico no início:
+
+var c = 1
+while (c <= 6){
+    console.log('Tudo bem?')
+    c++
+}
+
+/*
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+*/
+
+var n = 2000
+while (n <= 2022){
+    console.log(`Ano ${c}`)
+    c++
+} 
+
+// do while: estrutura de repetição com teste lógico no final:
+
+var c = 1
+do {
+    console.log(`Passo ${c}`);
+    c++
+} while (c <= 6)
