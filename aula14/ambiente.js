0c6e3fe4cb924486ce7299b455900088c3e94215
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-29 15:07:29 -0600

    Add files via upload

---
+++ aula14/ambiente.js
@@ -0,0 +1,15 @@
+//------ while
+ var c = 1
+ while (c <= 5) {
+     console.log(c);
+     c++
+ }
+//------ do while
+ var c = 1
+ do {
+     console.log(c);
+ } while (c <= 5)
+//------ for
+for(var c = 1; c <= 5; c++) {
+    console.log(c);
+}
