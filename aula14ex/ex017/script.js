82425ef8d6a833f7c11560340f6bab822b2a544d
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-30 19:05:40 -0600

    Add files via upload

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,8 @@
-alert('Olá')
+function calcular(){
+    var num = document.getElementById('num')
+    var tab = document.getElementById('tab')
+
+    for (c=0; c<=10; tab*=c){
+        Number(num.value)
+    }
+}
