af3f3b4e7776367f6b19bb49287c6484fa77d632
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-06-05 20:31:01 -0300

    Add files via upload

---
+++ aula16ex/ex018/script.js
@@ -0,0 +1,28 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        window.alert('Tudo Ok!')
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+}
