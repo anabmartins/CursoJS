417de009afefbb98f0985bc428af2694be446f29
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-06-16 16:57:48 -0300

    Update objeto01.js

--- aula17/objeto01.js
+++ aula17/objeto01.js
@@ -9,3 +9,22 @@ engordar(p=0){
 amigo.engordar(2)
 console.log(`${amigo.nome} pesa ${amigo.peso}Kg`);
 console.log(typeof amigo);
+
+const fruits = [
+    {
+        name: 'Orange',
+        quantity: 23,
+    },
+    {
+        name:'Strawberry',
+        quantity: 20,
+    },
+    {
+        name:'Grape',
+        quantity: 22,
+    },
+];
+
+console.table(fruits)
+// recebe um array ou object e cada elemento 
+//ou propriedade enumerada, será uma linha da sua tabela
